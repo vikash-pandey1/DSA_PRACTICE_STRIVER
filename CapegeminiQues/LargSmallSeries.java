fd2d3079ab18ef153c5e252df946ee8ac02b466d
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-09-03 12:41:36 +0530

    add ques in capgemini

--- CapegeminiQues/LargSmallSeries.java
+++ CapegeminiQues/LargSmallSeries.java
@@ -3,37 +3,39 @@ package CapegeminiQues;
 import java.util.Arrays;
 
 public class LargSmallSeries {
-    public static void printSeries(int arr[],int n){
-        int larg=0,small=0;
-        if(n%2==0){
-            larg=n/2;
-            small=n-larg;
-        }else{
-            small=n/2;
-            larg = n-small;
+    public static void printSeries(int arr[], int n) {
+        Arrays.sort(arr); // Sort the array in ascending order
+
+        int[] result = new int[n]; 
+        int larg = n - 1;  
+        int small = 0;    
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                result[i] = arr[larg--]; 
+            } else {
+                result[i] = arr[small++]; 
+            }
         }
-        Arrays.sort(arr);
-        int a=0;
-        for(int i=n-1;i>larg;i--){
-            arr[a]=arr[n];
-            a+=2;
+        for (int i = 0; i < n; i++) {
+            arr[i] = result[i];
         }
-        int b =1;
-        for(int i=0;i<small;i++){
-            arr[b]=arr[i];
-            b+=2;
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
+
     public static void main(String[] args) {
-        int arr[]={2,5,3,7,6};
+        int arr[] = {2, 5, 3, 7, 6};
         int n = arr.length;
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+
+        System.out.println("Original array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
+
+        System.out.println("Rearranged array:");
         printSeries(arr, n);
     }
 }
