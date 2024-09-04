d953c5bf82e0999c0dc1de2616243f33b5430748
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-09-04 09:05:01 +0530

    add ques compress string in capgemini

---
+++ CapegeminiQues/SttringCompress.java
@@ -0,0 +1,43 @@
+package CapegeminiQues;
+
+public class SttringCompress {
+    public static String compress(String str){
+        StringBuilder ans = new StringBuilder();
+        int count=1;
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==str.charAt(i-1)){
+                count++;
+            }else{
+                ans.append(str.charAt(i-1));
+                ans.append(count);
+                count=1;
+            }
+        }
+        ans.append(str.charAt(str.length()-1));
+        ans.append(count);
+
+        return ans.toString();
+    }
+    public static String compress1(String str){
+        StringBuilder ans = new StringBuilder();
+        int count=1;
+        for(int i=0;i<str.length()-1;i++){
+            if(str.charAt(i)==str.charAt(i+1)){
+                count++;
+            }else{
+                ans.append(str.charAt(i));
+                ans.append(count);
+                count=1;
+            }
+        }
+        ans.append(str.charAt(str.length()-1));
+        ans.append(count);
+
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String str = "aabbbbcccdeeeee";
+        System.out.println(compress(str));
+        System.out.println(compress1(str));
+    }
+}
